0892b15d4bb8b313b0abda1d201144c80216392d
Michael Vermeulen <mike.vermeulen9@gmail.com>
2016-06-12 15:06:07 +1200

    Added backend for openDyslexia

---
+++ Assets/Scripts/Accessibility/AccessibilityOptions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AccessibilityOptions : MonoBehaviour {
+    private bool dyslexicFont = false;
+
+    public void SetDyslexicFont (bool val)
+    {
+        dyslexicFont = val;
+    }
+
+    public bool GetDyslexicFont ()
+    {
+        return dyslexicFont;
+    }
+}
